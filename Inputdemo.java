298026bff39b4c1f24c3274c6d64335526dceba8
Hadiul Alam Hredoy <hadiulalam284@gmail.com>
2024-07-01 01:09:53 +0600

    Add files via upload

---
+++ Inputdemo.java
@@ -0,0 +1,24 @@
+
+package begginnerjava;
+
+import java.util.Scanner;
+
+public class Inputdemo {
+    
+    public static void main(String []args){
+        
+    
+         Scanner input = new Scanner(System.in);
+         double num1;
+             System.out.println("Enter any double vaule : " );
+
+           num1 = input.nextDouble();
+
+           System.out.println("num1");
+
+        
+    }
+    
+    
+    
+}
